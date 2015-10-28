4efa8e0547a314bda9c18c4cf0e09436f249c953
Edwardjojo <zhenyang.wu10@gmail.com>
2015-10-28 12:07:10 -0400

    CNY70 cal speed
    improve code, print the speed in serial monitor

---
+++ calWheelSpeed/CNY70calspeedV1.1/CNY70calspeedV1.1.ino
@@ -0,0 +1,41 @@
+int pinReceptor = A1;
+int sensorVal;
+int black = 0;
+double counter = 0;
+int timer = 0;
+double rotation = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  black = 0;
+  timer++;
+  sensorVal = analogRead(pinReceptor);
+  //Serial.println(sensorVal);
+  if(sensorVal < 100)  //the analog output is less than 100 when 
+  {                    //sensor detects black object
+    black = 1;
+  }
+  
+  if(black == 1)
+  {
+    counter++;
+    //Serial.print("counter:");
+    //Serial.println(counter);
+  }
+  //Serial.println(black);
+  if(timer == 20)    //print the speed of wheel every sec(20*50=1000ms)
+  {
+    rotation = counter/3.00;
+    Serial.print("the wheel speed: ");
+    Serial.print(rotation*15*2*3.14);
+    Serial.println("cm/sec");
+    timer = 0;
+    counter = 0;
+  }
+  delay(50);
+}
