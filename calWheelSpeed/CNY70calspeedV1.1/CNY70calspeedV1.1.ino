17f6e4a616c5f63b51ebb1ee8150b4a2f2840190
Edwardjojo <zhenyang.wu10@gmail.com>
2015-10-28 16:49:36 -0400

    improve accuracy

--- calWheelSpeed/CNY70calspeedV1.1/CNY70calspeedV1.1.ino
+++ calWheelSpeed/CNY70calspeedV1.1/CNY70calspeedV1.1.ino
@@ -15,8 +15,8 @@ void loop() {
   black = 0;
   timer++;
   sensorVal = analogRead(pinReceptor);
-  //Serial.println(sensorVal);
-  if(sensorVal < 100)  //the analog output is less than 100 when 
+  Serial.println(sensorVal);
+  if(sensorVal > 100)  //the analog output is less than 100 when 
   {                    //sensor detects black object
     black = 1;
   }
@@ -30,7 +30,7 @@ void loop() {
   //Serial.println(black);
   if(timer == 20)    //print the speed of wheel every sec(20*50=1000ms)
   {
-    rotation = counter/3.00;
+    rotation = counter/2.00;
     Serial.print("the wheel speed: ");
     Serial.print(rotation*18*3.14);
     Serial.println("cm/sec");
